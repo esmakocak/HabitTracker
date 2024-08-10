4e77183df2618605cb5965251a2bc8e7173d6a79
Esma Koçak <esma.kocaakk20@gmail.com>
2024-08-10 19:10:19 +0300

    project setup and entity creation

---
+++ HabitTracker/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  HabitTracker
+//
+//  Created by Esma Koçak on 10.08.2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
